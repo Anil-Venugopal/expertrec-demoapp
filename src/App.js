878ca24f5a404a897e49172ecd681b2989f2153a
geekysource <anil.vk@academicconnect.in>
2021-10-27 17:11:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './components/search.css'
+function App() {
+  return (
+    <div className="App">
+      <div className="search-header">
+      <input type="text" placeholder="search . . ." />
+      </div>
+    </div>
+  );
+}
+
+export default App;
